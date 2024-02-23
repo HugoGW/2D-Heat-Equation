f3821b0c3c0114ec8274408ecc8c2e3afd38c42c
Hugo ALEXANDRE <hugoalexandre127@gmail.com>
2024-02-23 14:54:40 +0100

    Update heat equation 2D simulation.py

--- heat equation 2D simulation.py
+++ heat equation 2D simulation.py
@@ -2,29 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Parameters
-a = 150
-length = 100
+D = 150  #Thermal diffusion coefficient
+Lx = 100  #length
+Ly = 100  #length
 time = 60
-nodes = 120
+Nx = 120  #space nodes
+Ny = 120  #space nodes
 
-dx, dy = length / nodes, length / nodes
-dt = min(dx**2 / (4 * a), dy**2 / (4 * a))
-t_nodes = int(time / dt)
+dx, dy = Lx / Nx, Ly / Ny  #space steps
+dt = min(dx**2 / (4 * D), dy**2 / (4 * D))  #time step
+Nt = int(time / dt)  #time nodes
 
 T_max, T_min = 100, 0
 
-center_x, center_y = nodes // 2, nodes // 2
+center_x, center_y = Nx // 2, Ny // 2
 R = 10  # Radius of the circle
 
 # Initialization
-T = np.zeros((nodes, nodes)) + 20  # Initial temperature of the plate : 15°C
-T[(np.arange(nodes)[:, np.newaxis] - center_x)**2 + (np.arange(nodes) - center_y)**2 == R**2] = 100
+T = np.zeros((Nx, Ny)) + 20  # Initial temperature of the plate : 15°C
+T[(np.arange(Nx)[:, np.newaxis] - center_x)**2 + (np.arange(Ny) - center_y)**2 == R**2] = 100
 
 # Boundary conditions
-T[0, :] = np.linspace(T_min, T_max, nodes)
-T[-1, :] = np.linspace(T_min, T_max, nodes)
-T[:, 0] = np.linspace(T_min, T_max, nodes)
-T[:, -1] = np.linspace(T_min, T_max, nodes)
+T[0, :] = np.linspace(T_min, T_max, Ny)
+T[-1, :] = np.linspace(T_min, T_max, Ny)
+T[:, 0] = np.linspace(T_min, T_max, Nx)
+T[:, -1] = np.linspace(T_min, T_max, Nx)
 
 # Plotting
 fig, ax = plt.subplots(figsize=(10,8))
@@ -34,14 +36,14 @@ plt.colorbar(pcm, ax=ax)
 # Simulation
 counter = 0
 while counter < time:
-    for i in range(1, nodes - 1):
-        for j in range(1, nodes - 1):
+    for i in range(1, Nx - 1):
+        for j in range(1, Ny - 1):
             d2x = (T[i - 1, j] - 2 * T[i, j] + T[i + 1, j]) / dx**2
             d2y = (T[i, j - 1] - 2 * T[i, j] + T[i, j + 1]) / dy**2
-            T[i, j] = dt * a * (d2x + d2y) + T[i, j]
+            T[i, j] = dt * D * (d2x + d2y) + T[i, j]
 
     counter += dt
-    T[(np.arange(nodes)[:, np.newaxis] - center_x)**2 + (np.arange(nodes) - center_y)**2 == R**2] = 100
+    T[(np.arange(Nx)[:, np.newaxis] - center_x)**2 + (np.arange(Ny) - center_y)**2 == R**2] = 100
     pcm.set_array(T)
     ax.set_title("Time: {:.3f} s, Avg Temperature: {:.2f}".format(counter, np.average(T)))
     plt.pause(0.01)
